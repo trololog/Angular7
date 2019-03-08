5ac39d8812db3d44af47eb8727a17ed8c66f35c9
trololog <dgmr85@gmail.com>
2019-03-08 17:08:48 -0500

    Adding files
    Adding files

---
+++ CookingRecipies/CookingRecipiesApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Test','https://hips.hearstapps.com/del.h-cdn.co/assets/18/06/1600x800/landscape-1517928338-delish-mongolian-ramen-and-meatballs-still001.jpg?resize=640:*')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
